cf96826f4e08b895c0d90d6210fb50b982279e79
Ivan Terekhov <for-spam32@yandex.ru>
2023-10-13 17:06:23 +0400

    feat: make a start menu for the bot
    SB-lessons

--- main.py
+++ main.py
@@ -16,61 +16,63 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 
-
 # TOKEN = getenv("toren")  # для запуска на https://replit.com
 TOKEN = tokenfile.token  # для оффлайна
 
 dp = Dispatcher()
 
-
-
-# Добавление обычных кнопок (т.е. таких, которые транслируют в чат то, что на них написано)
-@dp.message(Command("reply_builder"))
-async def reply_builder(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    for i in range(1, 17):
-        builder.add(types.KeyboardButton(text=str(i)+" действие такое-то"))
-    builder.adjust(4)
-    await message.answer(
-        "Выберите число:",
-        reply_markup=builder.as_markup(resize_keyboard=True),
-    )
-
-
-# функция set_button_handler вызывается только при получении
-# callback-запросов, у которых callback_data начинается с "set:":
-@dp.callback_query(lambda c: c.data.startswith('set:'))  # TODO заменить лямбда-функцию на F-фильтр!
-async def set_button_handler(callback_query: types.CallbackQuery) -> None:
-    index = int(callback_query.data.split(':')[1])
-    #TODO понять, как слать сообщение о нажатой кнопке непосредственно в чат, а не только как callback_query.answer():
-    await callback_query.answer(chat_id=callback_query.from_user.id, text=f"Вы нажали на кнопку {index}")
+main_menu = [
+    'Ароматы',
+    'О нас',
+    'Где затестить',
+    'Объемы',
+    'Доставка',
+    'Не только парфюм'
+]
 
 
 @dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")  # простое сообщение в чат
-    await message.reply("Как твои дела?")  # сообщение "в ответ", с цитированием реплики пользователя
+async def command_start_handler(message: Message, reply_ms=False, reply_cb=False) -> None:
+    if reply_ms:
+        await message.reply(f"Hello, {hbold(message.from_user.full_name)}!")
+    else:
+        await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
-    builder = InlineKeyboardBuilder()
-    for index in range(1, 11):
-        builder.button(text=f"Кнопка {index}", callback_data=f"set:{index}")
-    builder.adjust(3, 2, 3)
+    if reply_cb:
+        builder, another_builder = ReplyKeyboardBuilder(), ReplyKeyboardBuilder()
+    else:
+        builder, another_builder = InlineKeyboardBuilder(), InlineKeyboardBuilder()
 
-    another_builder = InlineKeyboardBuilder()
-    for index in range(11, 21):
-        another_builder.button(text=f"Кнопка {index}", callback_data=f"set:{index}")
-    another_builder.adjust(1, 2, 3, 4)
-    builder.attach(another_builder)  # присоседим another_builder к builder
+    for option in main_menu:
+        builder.button(text=option, callback_data=f"set:{option}")  # TODO далее - написать хэндлер калбеков
+    builder.adjust(1, 2, 2, 1)
 
-    await message.answer("Some text here", reply_markup=builder.as_markup())
+    for index in range(11, 13):
+        option = f"Еще что-то {index}"
+        another_builder.button(text=option, callback_data=f"set:{option}")
+    another_builder.button(text='Кушоц', callback_data='Кушоц')
+    another_builder.adjust(3)
+    builder.attach(another_builder)
+
+    await message.answer(
+        "Я - арома-бот, помогу Вам с подбором аромата! Вжуух!",
+        reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
+    )
 
 
-@dp.message(Command("pure"))
-async def pure(message: types.Message):
+@dp.callback_query(F.data.startswith('set:'))
+async def set_button_handler(callback_query: types.CallbackQuery) -> None:
+    user_react = callback_query.data.split(':')[1]
+    await callback_query.message.answer(text=f"Вы выбрали пункт '{user_react}'! Сейчас я всё расскажу! ;)")
+
+
+#TODO заменить обычную клавиатуру на инлайн:
+@dp.callback_query(F.data == 'Кушоц')
+async def pure(callback_query: types.CallbackQuery) -> None:
     kb = [
         [
-        types.KeyboardButton(text="С пюрешкой"),
-        types.KeyboardButton(text="Без пюрешки")
+            types.KeyboardButton(text="С пюрешкой"),
+            types.KeyboardButton(text="Без пюрешки")
         ]
     ]
     keyboard = types.ReplyKeyboardMarkup(
@@ -78,37 +80,42 @@ async def pure(message: types.Message):
         resize_keyboard=True,
         input_field_placeholder="Выберите способ подачи"
     )
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+    await callback_query.message.answer(text="Как подавать котлеты?", reply_markup=keyboard)
 
 
 @dp.message(F.text.lower() == "с пюрешкой")
 async def with_puree(message: types.Message):
-    await message.reply("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message(F.text.lower() == "без пюрешки")
 async def without_puree(message: types.Message):
-    await message.reply("Так невкусно!", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer("Так невкусно!", reply_markup=types.ReplyKeyboardRemove())
 
 
-# ловим определенный ответ ("Ок"):
-@dp.message()
-async def how_are_you(message: Message) -> None:
-    text = message.text  # Получение текста из сообщения
-    user_id = message.from_user.id  # Получение ID пользователя
-    if text == "Ок":
-        await message.answer(f"Хорошо!")
-    else:
-        await message.answer(f"Похоже, ты, {user_id}, не ответил на мой вопрос.")
+# Специальные обычные кнопки позволяют запросить гео и контакт юзера:
+@dp.message(Command("special_buttons"))
+async def cmd_special_buttons(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(
+        types.KeyboardButton(text="Запросить геолокацию", request_location=True),
+        types.KeyboardButton(text="Запросить контакт", request_contact=True),
+    )
+
+    await message.answer(
+        "Выберите действие:",
+        reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True),
+    )
 
 
-# эхо-функция:
+# эхо-функция
+#TODO сделать эхо только текста
 @dp.message()
 async def echo_handler(message: Message) -> None:
     try:
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        await message.answer("Nice try!")
+        await message.answer("Хорошая попытка! Попробуйте еще раз!")
 
 
 async def main() -> None:
