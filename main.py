b1eeba207db81abad45425974f03255a25331644
Ivan Terekhov <for-spam32@yandex.ru>
2023-10-13 20:33:33 +0400

    feat: try out links and replies to messages
    SB-lessons

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import tokenfile  # для оффлайна
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.utils.markdown import hbold
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
@@ -27,86 +27,85 @@ main_menu = [
     'Где затестить',
     'Объемы',
     'Доставка',
-    'Не только парфюм'
+    'Не только парфюм',
 ]
 
 
 @dp.message(CommandStart())
-async def command_start_handler(message: Message, reply_ms=False, reply_cb=False) -> None:
-    if reply_ms:
-        await message.reply(f"Hello, {hbold(message.from_user.full_name)}!")
-    else:
-        await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {hbold(message.from_user.full_name)}!", reply_markup=ReplyKeyboardRemove())
 
-    if reply_cb:
-        builder, another_builder = ReplyKeyboardBuilder(), ReplyKeyboardBuilder()
-    else:
-        builder, another_builder = InlineKeyboardBuilder(), InlineKeyboardBuilder()
-
+    builder, another_builder = InlineKeyboardBuilder(), InlineKeyboardBuilder()
     for option in main_menu:
-        builder.button(text=option, callback_data=f"set:{option}")  # TODO далее - написать хэндлер калбеков
+        builder.button(text=option, callback_data=f"set:{option}")
     builder.adjust(1, 2, 2, 1)
 
-    for index in range(11, 13):
-        option = f"Еще что-то {index}"
-        another_builder.button(text=option, callback_data=f"set:{option}")
-    another_builder.button(text='Кушоц', callback_data='Кушоц')
+    another_builder = InlineKeyboardBuilder()
+    another_builder.button(text='Кушоц', callback_data='кушоц')
+    another_builder.button(text='Спец', callback_data='спец')
+    another_builder.button(text='Instagram', url="https://instagram.com/cloud.parfume?igshid=MzRlODBiNWFlZA==")
     another_builder.adjust(3)
     builder.attach(another_builder)
-
     await message.answer(
-        "Я - арома-бот, помогу Вам с подбором аромата! Вжуух!",
-        reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
+        "Я - арома-бот, помогу с подбором аромата! Вжуух!",
+        reply_markup=builder.as_markup(resize_keyboard=True)
     )
 
 
 @dp.callback_query(F.data.startswith('set:'))
 async def set_button_handler(callback_query: types.CallbackQuery) -> None:
     user_react = callback_query.data.split(':')[1]
-    await callback_query.message.answer(text=f"Вы выбрали пункт '{user_react}'! Сейчас я всё расскажу! ;)")
-
-
-#TODO заменить обычную клавиатуру на инлайн:
-@dp.callback_query(F.data == 'Кушоц')
-async def pure(callback_query: types.CallbackQuery) -> None:
-    kb = [
-        [
-            types.KeyboardButton(text="С пюрешкой"),
-            types.KeyboardButton(text="Без пюрешки")
-        ]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите способ подачи"
+    await callback_query.message.answer(
+        reply_markup=ReplyKeyboardRemove(),
+        text=f"Вы выбрали пункт '{user_react}'! Сейчас я всё расскажу! ;)",
     )
-    await callback_query.message.answer(text="Как подавать котлеты?", reply_markup=keyboard)
 
 
-@dp.message(F.text.lower() == "с пюрешкой")
-async def with_puree(message: types.Message):
-    await message.answer("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
+@dp.callback_query(F.data == 'кушоц')
+async def pure(callback_query: types.CallbackQuery) -> None:
+    keyboard_inline = InlineKeyboardBuilder()
+    keyboard_inline.button(text='С пюрешкой', callback_data='с пюрешкой')
+    keyboard_inline.button(text='Без пюрешки', callback_data='без пюрешки')
+    keyboard_inline.adjust(2)
+    await callback_query.message.answer(
+        text="Как подавать котлеты?",
+        reply_markup=keyboard_inline.as_markup(resize_keyboard=True)
+    )
 
 
-@dp.message(F.text.lower() == "без пюрешки")
-async def without_puree(message: types.Message):
-    await message.answer("Так невкусно!", reply_markup=types.ReplyKeyboardRemove())
+@dp.callback_query(F.data == 'с пюрешкой')
+async def with_puree(callback_query: types.CallbackQuery) -> None:
+    await callback_query.message.answer("Отличный выбор!")
 
+
+@dp.callback_query(F.data == 'без пюрешки')
+async def without_puree(callback_query: types.CallbackQuery) -> None:
+    await callback_query.message.answer("Так невкусно!")
+
+
+#TODO в процессе использования выяснилось, что при нажатии на кнопки с request_location и request_contact
+# не срабатывает one_time_keyboard. В качестве костыля предлагается во всех хендлерах прописывать
+# аргумент для answer => reply_markup=ReplyKeyboardRemove() либо добавлять кнопку-пустышку
 
 # Специальные обычные кнопки позволяют запросить гео и контакт юзера:
-@dp.message(Command("special_buttons"))
-async def cmd_special_buttons(message: types.Message):
+@dp.callback_query(F.data == 'спец')
+async def cmd_special_buttons(callback_query: types.CallbackQuery) -> None:
     builder = ReplyKeyboardBuilder()
-    builder.row(
-        types.KeyboardButton(text="Запросить геолокацию", request_location=True),
-        types.KeyboardButton(text="Запросить контакт", request_contact=True),
-    )
-
-    await message.answer(
+    builder.button(text="Отправить геолокацию", request_location=True),
+    builder.button(text="Отправить контакт", request_contact=True),
+    builder.button(text='Убрать клавиатуру')
+    await callback_query.message.answer(
         "Выберите действие:",
-        reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True),
+        reply_markup=builder.as_markup(
+            resize_keyboard=True,
+            one_time_keyboard=True
+        )
     )
 
+
+@dp.message(F.text == 'Убрать клавиатуру')
+async def del_message(message: Message) -> None:
+    await message.answer(text='Специальная клавиатура убрана', reply_markup=ReplyKeyboardRemove())
 
 # эхо-функция
 #TODO сделать эхо только текста
