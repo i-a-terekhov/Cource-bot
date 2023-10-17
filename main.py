690c18aa691be9c22800f90802bb3132ace3bc96
Ivan Terekhov <for-spam32@yandex.ru>
2023-10-17 22:42:55 +0400

    feat: output "typing text" while the bot is running
    SB-lessons

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 from aiogram.exceptions import TelegramBadRequest
-
+from aiogram.utils.chat_action import ChatActionSender
 
 # TOKEN = getenv("toren")  # для запуска на https://replit.com
 TOKEN = tokenfile.token  # для оффлайна
@@ -42,7 +42,7 @@ async def delete_message(message: types.Message):
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await delete_message(message)  # удаляет сообщение пользователя (т.е. '\start', в данном случае)
+    # await delete_message(message)  # удаляет сообщение пользователя (т.е. '\start', в данном случае)
     await message.answer(
         f"Привет, {hbold(message.from_user.full_name)}!",
         reply_markup=ReplyKeyboardRemove()
@@ -68,34 +68,39 @@ async def command_start_handler(message: Message) -> None:
 
 
 @dp.callback_query(F.data.startswith('set:'))
-async def set_button_handler(callback_query: types.CallbackQuery) -> None:
-    user_react = callback_query.data.split(':')[1]
-    await callback_query.message.answer(
+async def set_button_handler(callback: types.CallbackQuery) -> None:
+    await callback.answer()  # Данный callback.answer() нужен только для того, чтобы на андройде
+    # не проигрывалась анимация ожидания у нажатой кнопки (белый волнообразный градиент). У яблока такого нет
+    user_react = callback.data.split(':')[1]
+    await callback.message.answer(
         reply_markup=ReplyKeyboardRemove(),
         text=f"Вы выбрали пункт '{user_react}'! Сейчас я всё расскажу! ;)",
     )
 
 
 @dp.callback_query(F.data == 'кушоц')
-async def pure(callback_query: types.CallbackQuery) -> None:
+async def pure(callback: types.CallbackQuery) -> None:
+    await callback.answer()
     keyboard_inline = InlineKeyboardBuilder()
     keyboard_inline.button(text='С пюрешкой', callback_data='с пюрешкой')
     keyboard_inline.button(text='Без пюрешки', callback_data='без пюрешки')
     keyboard_inline.adjust(2)
-    await callback_query.message.answer(
+    await callback.message.answer(
         text="Как подавать котлеты?",
         reply_markup=keyboard_inline.as_markup(resize_keyboard=True)
     )
 
 
 @dp.callback_query(F.data == 'с пюрешкой')
-async def with_puree(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer("Отличный выбор!")
+async def with_puree(callback: types.CallbackQuery) -> None:
+    await callback.answer()
+    await callback.message.answer("Отличный выбор!")
 
 
 @dp.callback_query(F.data == 'без пюрешки')
-async def without_puree(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer("Так невкусно!")
+async def without_puree(callback: types.CallbackQuery) -> None:
+    # await callback.answer()
+    await callback.message.answer("Так невкусно!")
 
 
 # В процессе использования выяснилось, что при нажатии на кнопки с request_location и request_contact
@@ -103,12 +108,13 @@ async def without_puree(callback_query: types.CallbackQuery) -> None:
 # аргумент для answer => reply_markup=ReplyKeyboardRemove() либо добавлять кнопку-пустышку
 # Специальные обычные кнопки позволяют запросить гео и контакт юзера:
 @dp.callback_query(F.data == 'спец')
-async def cmd_special_buttons(callback_query: types.CallbackQuery) -> None:
+async def cmd_special_buttons(callback: types.CallbackQuery) -> None:
+    await callback.answer()
     builder = ReplyKeyboardBuilder()
     builder.button(text="Отправить геолокацию", request_location=True),
     builder.button(text="Отправить контакт", request_contact=True),
     builder.button(text='Убрать клавиатуру')
-    await callback_query.message.answer(
+    await callback.message.answer(
         "Выберите действие:",
         reply_markup=builder.as_markup(
             resize_keyboard=True,
@@ -137,6 +143,7 @@ async def cmd_random(message: types.Message):
 
 @dp.callback_query(F.data == "random_value")
 async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer()
     await callback.message.answer(str(randint(1, 10)))
 
     # await callback.message.answer(callback.from_user.full_name)
@@ -147,8 +154,6 @@ async def send_random_value(callback: types.CallbackQuery):
     #     string_items += name + ': ' + str(val) + '\n'
     # await callback.message.answer(string_items)
 
-    # Если вызывать callback.message.answer на андройде, кнопка "Нажми меня" будет 30 сек. анимироваться как ожидающая
-    # ответа. Но если добавить callback.answer(), анимация не наблюдается:
     await callback.answer(
         text="Спасибо, что воспользовались генератором случайных целых чисел от одного до десяти!\n"
              "В следующей версии генератор сможет выдать целые числа от одного до ДВАДЦАТИ!",
@@ -157,41 +162,45 @@ async def send_random_value(callback: types.CallbackQuery):
 
 
 @dp.message(Command('clear'))
-async def cmd_clear(message: types.Message, bot: Bot, forward: None) -> None:
-    forward = True
-    deleted_message = []
-    print(f'Запрошено удаление сообщений в чате с id={message.chat.id}')
-    # В данной конфигурации цикл продолжается последние 50 номеров для сообщений, это могут быть как "сообщения",
-    # так и "удаленные сообщения". Сообщения, написанные после кнопки "Очистить чат" - остаются.
-    for i in range(message.message_id, message.message_id - 50, -1):
-        try:
-            ex_message = await bot.forward_message(
+async def cmd_clear(message: types.Message, bot: Bot, del_forward: None) -> None:
+    # выводит "печатает" во время работы функции:
+    async with ChatActionSender.typing(bot=bot, chat_id=message.chat.id, interval=3.0):
+        deleted_message = []
+        del_forward = False
+        if not del_forward:
+            deleted_message = ['функция сохранения удаляемых сообщений была отключена']
+        print(f'Запрошено удаление сообщений в чате с id={message.chat.id}')
+
+        for i in range(message.message_id, message.message_id - 50, -1):
+            try:
+                if del_forward:
+                    # пересылка сообщений - это "костыль", позволяющий сохранить текст сообщений:
+                    ex_message = await bot.forward_message(
                         chat_id=message.chat.id,
                         from_chat_id=message.chat.id,
                         message_id=i)
-            if ex_message:
+                    if ex_message:
+                        deleted_message.append(ex_message.text)
+                        await bot.delete_message(message.chat.id, ex_message.message_id)
                 await bot.delete_message(message.chat.id, i)
-                print(f'Удалено сообщение {i}: {ex_message.text}')
-                deleted_message.append(ex_message.text)
-                if forward:
-                    await bot.delete_message(message.chat.id, ex_message.message_id)
-        except:
-            pass
-    print('Удалены сообщения', deleted_message)
-    await message.answer('/start')
+            except:
+                pass
+        print('Удалены сообщения', deleted_message)
+        # await message.answer('/start')
 
 
 @dp.callback_query(F.data == "clear")
 async def clear(callback: types.CallbackQuery):
+    await callback.answer()
     await cmd_clear(callback.message, dp.bot, None)
 
 
 # эхо-функция
-# TODO сделать эхо только текста
 @dp.message()
 async def echo_handler(message: Message) -> None:
     try:
-        await message.send_copy(chat_id=message.chat.id)
+        if types.ContentType.TEXT == message.content_type:
+            await message.send_copy(chat_id=message.chat.id)
     except TypeError:
         await message.answer("Хорошая попытка! Попробуйте еще раз!")
 
