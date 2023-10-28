e62b98a4e49e5a9b0549d715469a2521f4c112cd
Ivan Terekhov <for-spam32@yandex.ru>
2023-10-28 23:28:00 +0400

    feat: write a filter that gives arguments to the handler
    SB-lessons

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from handlers import group_games, questions, different_types
+from handlers import group_games, usernames, questions, different_types
 
 import tokenfile
 
@@ -12,7 +12,7 @@ async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
-    dp.include_routers(group_games.router, questions.router, different_types.router)
+    dp.include_routers(group_games.router, usernames.router, questions.router, different_types.router)
 
     # Альтернативный вариант регистрации роутеров по одному на строку
     # dp.include_router(questions.router)
