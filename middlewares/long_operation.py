bcf37939769bbab8af487c18ab328895971b910f
Ivan Terekhov <for-spam32@yandex.ru>
2023-10-30 23:54:43 +0400

    feat: add prints to track the dispatcher's work
    SB-lessons

--- middlewares/long_operation.py
+++ middlewares/long_operation.py
@@ -7,6 +7,8 @@ from aiogram.types import Message
 from aiogram.dispatcher.flags import get_flag
 from aiogram.utils.chat_action import ChatActionSender
 
+dict_operations = {}
+
 
 class ChatActionMiddleware(BaseMiddleware):
     async def __call__(
@@ -15,7 +17,18 @@ class ChatActionMiddleware(BaseMiddleware):
         event: Message,
         data: Dict[str, Any]
     ) -> Any:
-        print(f'работает мидлварь long_operation. {handler}')
+        print(f'работает мидлварь long_operation')
+        for key, value in data.items():
+            if dict_operations.get(key):
+                if dict_operations[key] == value:
+                    print(f'{key:20} не изменился')
+                else:
+                    print(f'{key:20} -->X {dict_operations[key].__dict__}\n{key:20} ~~~> {value.__dict__}')
+                    dict_operations[key] = value
+            else:
+                dict_operations[key] = value
+                print(f'{key:20} ---> {value}')
+
         long_operation_type = get_flag(data, "long_operation")
         # pprint(data)
         print(f'\tлонг оператион тайп {long_operation_type}')
