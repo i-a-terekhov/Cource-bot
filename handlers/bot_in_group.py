69e913af61590e372cd8ed0dcb9d776530eddabe
Ivan Terekhov <for-spam32@yandex.ru>
2023-11-05 14:10:05 +0400

    fix: add a waiting function to exclude an error
    if, when the handler is triggered to change the status of the bot in the group, he immediately tries to write something, an exception appears "the bot is not a member of the group"
    
    SB-lessons

--- handlers/bot_in_group.py
+++ handlers/bot_in_group.py
@@ -1,4 +1,7 @@
-from aiogram import F, Router
+from asyncio import sleep
+from pprint import pprint
+
+from aiogram import F, Router, Bot
 from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, IS_NOT_MEMBER, KICKED, MEMBER, ADMINISTRATOR, IS_MEMBER
 from aiogram.types import ChatMemberUpdated
 
@@ -6,40 +9,37 @@ from aiogram.filters.chat_member_updated import \
     ChatMemberUpdatedFilter, JOIN_TRANSITION
 
 router = Router()
-router.chat_member.filter(F.chat.type.in_({'group', 'supergroup'}))
-
-
-@router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=JOIN_TRANSITION))
-async def wow(event: ChatMemberUpdated):
-    print('получилось?')
+router.chat_member.filter(F.chat.type.in_({'group', 'supergroup', 'channel'}))
 
 chats_variants = {
     "group": "группу",
-    "supergroup": "супергруппу"
+    "supergroup": "супергруппу",
+    "channel": "канал",
 }
 
 
-#TODO эта функция кидает исключения:
-@router.my_chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
-async def bot_added_as_member(event: ChatMemberUpdated):
-    print('Сработал фильтр на мембера')
+@router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> ADMINISTRATOR))
+async def bot_added_as_admin(event: ChatMemberUpdated):
+    print(f'Бот добавлен как админ в чат {event.chat.title}')
+    print(event.chat.type)
+    print(event.chat.title)
+    print(event.chat.id)
+    print(event.chat.permissions)
+    await sleep(3)
     await event.answer(
-        text=f"Привет! Спасибо, что добавили меня в "
-             # f'{chats_variants[event.chat.type]} "{event.chat.title}" '
-             # f"как администратора. ID чата: {event.chat.id}"
+        text=f"Привет! Спасибо, что добавили "
+             f"меня в {chats_variants[event.chat.type]} '{event.chat.title}' как администратора. "
+             f"ID чата: {event.chat.id}"
     )
 
 
-# @router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> MEMBER))
-# async def bot_added_as_member(event: ChatMemberUpdated):
-#     # Вариант посложнее: бота добавили как обычного участника.
-#     # Но может отсутствовать право написания сообщений, поэтому заранее проверим.
-#     chat_info = await bot.get_chat(event.chat.id)
-#     if chat_info.permissions.can_send_messages:
-#         await event.answer(
-#             text=f"Привет! Спасибо, что добавили меня в "
-#                  f'{chats_variants[event.chat.type]} "{event.chat.title}" '
-#                  f"как обычного участника. ID чата: {event.chat.id}"
-#         )
-#     else:
-#         print("Как-нибудь логируем эту ситуацию")
+# Не получилось проверить возможность написания комментов ботом:
+# Не получилось проверить реакцию на добавление бота как простого участника, т.к. в телеге не найдено такой возможности
+@router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> MEMBER))
+async def bot_added_as_member(event: ChatMemberUpdated):
+    await sleep(3)
+    await event.answer(
+        text=f"Привет! Спасибо, что добавили меня в "
+             f'{chats_variants[event.chat.type]} "{event.chat.title}" '
+             f"как обычного участника. ID чата: {event.chat.id}"
+    )
