ce9a25786e809a0dd7ead0310cc2bd320f545d1a
Ivan Terekhov <for-spam32@yandex.ru>
2023-11-08 23:11:48 +0400

    fix: record an unsuccessful launch attempt
    SB-lessons

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+from pprint import pprint
+
 from aiogram import Bot, Dispatcher
 from handlers import checkin, group_games, usernames, questions, different_types, in_pm, bot_in_group
 from middlewares.weekend import WeekendCallbackMiddleware
@@ -9,12 +11,15 @@ import tokenfile
 
 TOKEN = tokenfile.token
 OWNER_CHAT_ID = 5180054391
-OWNER_CHANNEL_ID = 1001998310331
+OWNER_CHANNEL_ID = 100209597300
+OWNER_GROUP_ID = 1001998310331
 bot_unit = Bot(token=TOKEN, parse_mode="HTML")
 
 
 # Запуск бота
 async def main(bot: Bot):
+    await bot.send_message(chat_id=OWNER_CHAT_ID, text='Привет!')
+    pprint(bot.__dir__())
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -34,7 +39,8 @@ async def main(bot: Bot):
     dp.callback_query.outer_middleware(WeekendCallbackMiddleware())
     # пропустить необработанные апдейты:
     # await bot.delete_webhook(drop_pending_updates=True)
-    admins = await bot.get_chat_administrators(OWNER_CHANNEL_ID)
+    #TODO перепроверить ID чатов:
+    admins = await bot.get_chat_administrators(chat_id=OWNER_CHANNEL_ID)
     admin_ids = {admin.user.id for admin in admins}
     await dp.start_polling(bot, admins=admin_ids, allowed_updates=["message", "inline_query", "chat_member"])
 
