716fa9f8c24b426e82b3195b006a215d31af68a6
Ivan Terekhov <for-spam32@yandex.ru>
2023-11-10 22:43:49 +0400

    feat: add the formation of a list of admins to the minibot
    SB-lessons

--- micro_bot.py
+++ micro_bot.py
@@ -15,8 +15,17 @@ async def main():
     # Чтобы отправить сообщение в чат, бот должен найти его.
     # Личку с юзером бот может найти только по ID, группы и каналы - по имени, и только если они публичные.
     await bot.send_message(chat_id=OWNER_CHAT_ID, text='Я родился!')
+
     await bot.send_message(chat_id=OWNER_GROUP_NAME, text='Привет, группа!')
+    admins_in_group = await bot.get_chat_administrators(chat_id=OWNER_GROUP_NAME)
+    admins = {admin.user.first_name for admin in admins_in_group}
+    print(f'Список админов чата {OWNER_GROUP_NAME}: {admins}')
+
     await bot.send_message(chat_id=OWNER_CHANNEL_NAME, text='Привет, канал!')
+    admins_in_channel = await bot.get_chat_administrators(chat_id=OWNER_CHANNEL_NAME)
+    admins = {admin.user.first_name for admin in admins_in_channel}
+    print(f'Список админов чата {OWNER_CHANNEL_NAME}: {admins}')
+
     await bot.session.close()
 
 if __name__ == "__main__":
