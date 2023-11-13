8beeb4d850f2f7b8ba9deb87205fa42229cabc2e
Ivan Terekhov <for-spam32@yandex.ru>
2023-11-13 21:03:57 +0400

    feat: fix the operability of the user rights check
    SB-lessons

--- bot.py
+++ bot.py
@@ -1,12 +1,7 @@
 import asyncio
 import logging
-import time
-from asyncio import sleep
-from pprint import pprint
-from threading import Thread
 
-from aiogram.types import ChatMember, User
-from aiogram import Bot, Dispatcher, exceptions
+from aiogram import Bot, Dispatcher
 from handlers import checkin, group_games, usernames, questions, different_types, in_pm, bot_in_group, admin_changes_in_group, events_in_group
 from middlewares.weekend import WeekendCallbackMiddleware
 from middlewares.long_operation import ChatActionMiddleware
@@ -14,18 +9,13 @@ from middlewares.long_operation import ChatActionMiddleware
 import tokenfile as tf
 
 TOKEN = tf.TOKEN
-TOKEN_TWO = tf.TOKEN_TWO
-TOKEN_THREE = tf.TOKEN_THREE
 OWNER_CHAT_ID = tf.OWNER_CHAT_ID
 OWNER_CHANNEL_ID = tf.OWNER_CHANNEL_ID
 OWNER_GROUP_NAME = tf.OWNER_GROUP_NAME
 
 bot_unit = Bot(token=TOKEN)
-bot_riot = Bot(token=TOKEN_TWO)
-bot_citizen = Bot(token=TOKEN_THREE)
 
 
-# TODO проблема заключается в том, что роутер можно подключить только к одному диспетчеру
 async def set_up(dp):
     # Порядок регистрации роутеров критичен. Апдейт, пойманный во втором роутере - не попадет в обработку к третьему:
     dp.include_routers(
@@ -71,15 +61,7 @@ async def main(bot: Bot):
 
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-
-    bots_farm = [bot_unit, bot_riot, bot_citizen]
-
-    loop = asyncio.get_event_loop()
-
-    tasks = [main(token) for token, in zip(bots_farm)]
-
-    loop.run_until_complete(asyncio.gather(*tasks))
+    asyncio.run(main(bot_unit))
 
 
 
